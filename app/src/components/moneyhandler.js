f602a3bbf9e60480c5b3584868507c8d53441844
Jonathan Barton <jonathan.barton@mheducation.com>
2015-04-21 00:19:08 -0400

    added gulp mithril setup and initial files

---
+++ app/src/components/moneyhandler.js
@@ -0,0 +1,9 @@
+var MoneyHandler = MoneyHandler || {};
+
+MoneyHandler.controller = function() {
+
+};
+
+MoneyHandler.view = function(){
+	return m("div");
+};
---
+++ app/src/main.js
@@ -0,0 +1,5 @@
+'use strict';
+//var _ = require('underscore');
+
+var MoneyHandler = require("./components/moneyhandler");
+m.module(document.getElementById("money-handler"), {controller: MoneyHandler.controller, view: MoneyHandler.view});
